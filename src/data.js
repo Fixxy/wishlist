21624b825862274b8feeaaf8e5fd86a7bfab3070
Fixxy <csszone@gmail.com>
2020-01-05 00:59:10 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -8,7 +8,7 @@ export let items = [
 	}, {
 		'img': 'https://static-eu.insales.ru/images/products/1/1787/230344443/large_logo_new2019.jpg',
 		'name': 'Подарочный сертификат в Zero Waste Shop',
-		'price': 'от 500 ₽',
+		'price': 'от 100 ₽',
 		'url': 'https://zws.moscow/collection/podarochnye-sertifikaty',
 		'description': 'тест-тест-тест222'
 	}
