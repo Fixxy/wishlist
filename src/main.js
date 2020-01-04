7b158e98424fcc8490ad4635dd56aa0aa936ee4e
Fixxy <csszone@gmail.com>
2020-01-05 00:29:57 +0300

    some more code

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import {items} from './data.js';
 
 console.log(items);
 
-for (let item in items) {
-	console.log(item);
-}
+let test = items;
+
+items.forEach(function(el){
+	console.log(el);
+	let wishlist = document.getElementById('wishlist_block');
+	let wish = document.createElement('div');
+		wish.setAttribute('class', 'wish_el');
+		wish.innerHTML(`${el.name} ; ${el.url} ; ${el.img} ; ${el.description}`);
+	wishlist.appendChild(wish);  
+});
