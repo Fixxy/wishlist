3e345b819800b2bb1a7d4b49296e812cc85e2875
Fixxy <csszone@gmail.com>
2020-01-05 15:09:45 +0300

    final touches this time?

--- src/data.js
+++ src/data.js
@@ -1,15 +1,13 @@
 export let items = [
 	{
-		'img': 'https://cdn1.ozone.ru/s3/multimedia-v/c1200/6006568483.jpg',
-		'name': 'Игра Клаустрафобия "Пробуждение"',
-		'price': '2349 ₽',
-		'url': 'https://www.ozon.ru/context/detail/id/163141508/',
-		'description': 'Игра квест-головоломка, в которой вам предстоит пройти захватывающую дух историю.'
+		"img": "https://cdn1.ozone.ru/s3/multimedia-v/c1200/6006568483.jpg",
+		"name": 'Игра Клаустрафобия "Пробуждение"',
+		"price": "2349 ₽",
+		"url": "https://www.ozon.ru/context/detail/id/163141508/"
 	}, {
-		'img': 'https://static-eu.insales.ru/images/products/1/1787/230344443/large_logo_new2019.jpg',
-		'name': 'Подарочный сертификат в Zero Waste Shop',
-		'price': 'от 100 ₽',
-		'url': 'https://zws.moscow/collection/podarochnye-sertifikaty',
-		'description': 'тест-тест-тест222'
+		"img": "https://static-eu.insales.ru/images/products/1/1787/230344443/large_logo_new2019.jpg",
+		"name": "Подарочный сертификат в Zero Waste Shop",
+		"price": "от 100 ₽",
+		"url": "https://zws.moscow/collection/podarochnye-sertifikaty"
 	}
 ]
--- src/main.js
+++ src/main.js
@@ -1,33 +1,20 @@
 import {items} from './data.js';
 
-console.log(items);
-
-let test = items;
-
 items.forEach(function(el){
 	console.log(el);
 	let wishlist = document.getElementById('wish_block');
-	let wish = document.createElement('div');
+	let wish = document.createElement('li');
 		wish.setAttribute('class', 'wish_el')
 		wish.innerHTML = return_fancy_html(el);
 	wishlist.appendChild(wish);  
 });
 
 function return_fancy_html(element) {
-	let html = `<div class="col-md-6 mb-4">
-					<div class="card">
-						<div class="view overlay">
-							<a href="${element.url}" target="_blank">
-								<img src="${element.img}" class="card-img-top" alt="">
-							</a>
-						</div>
-						<div class="card-body">
-							<h4 class="card-title">${element.name}</h4>
-							<!--Text-->
-							<p class="card-text">${element.description}</p>
-							<a href="#" target="_blank" class="btn btn-primary btn-md waves-effect waves-light">Открыть</a>
-						</div>
-					</div>
-				</div>`;
+
+
+	let html = `<a href="${element.url}" target="blank">
+					<div class="wish_el_photo" style="background-image: url(${element.img})"></div>
+					<div class="wish_el_name">${element.name}</div>
+				</a>`;
 	return html;
 }
