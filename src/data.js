5517d633053f0adefa3862b65e6c5e325e424994
Fixxy <csszone@gmail.com>
2020-01-05 16:13:41 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -6,8 +6,13 @@ export let items = [
 		"url": "https://www.ozon.ru/context/detail/id/163141508/"
 	}, {
 		"img": "https://static-eu.insales.ru/images/products/1/1787/230344443/large_logo_new2019.jpg",
-		"name": "Подарочный сертификат в Zero Waste Shop",
+		"name": 'Подарочный сертификат в Zero Waste Shop',
 		"price": "от 100 ₽",
 		"url": "https://zws.moscow/collection/podarochnye-sertifikaty"
+	}, {
+		"img": "https://hobbygames.cdnvideo.ru/image/cache/hobbygames_beta/data/HobbyWorld/Ostrov_Duhov/Photos/Ostrov_Duhov_01-1024x1024-wm.jpg",
+		"name": 'Настольная игра "Остров Духов"',
+		"price": "4500 ₽",
+		"url": "https://hobbygames.ru/ostrov-duhov?gclid=Cj0KCQiAr8bwBRD4ARIsAHa4YyIxec6KNV3-ON3TWU7Ioxj0ZEyAlqgTn1Cxkj34tcSbPXNIjlhK-2kaAn2bEALw_wcB"
 	}
 ]
