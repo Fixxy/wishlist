54f4c532698deee158a13d51d15c758513ecc9ad
Fixxy <csszone@gmail.com>
2020-01-05 15:54:32 +0300

    changed the structure a bit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import {items} from './data.js';
 
 items.forEach(function(el){
-	console.log(el);
 	let wishlist = document.getElementById('wish_block');
 	let wish = document.createElement('li');
 		wish.setAttribute('class', 'wish_el')
@@ -10,11 +9,12 @@ items.forEach(function(el){
 });
 
 function return_fancy_html(element) {
-
-
 	let html = `<a href="${element.url}" target="blank">
 					<div class="wish_el_photo" style="background-image: url(${element.img})"></div>
-					<div class="wish_el_name">${element.name}</div>
+					<div class="wish_el_text">
+						<div class="wish_el_name">${element.name}</div>
+						<div class="wish_el_price">${element.price}</div>
+					</div>
 				</a>`;
 	return html;
 }
