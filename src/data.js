67537cb5bf9d6ba44f806f28311f24fb8855d5d6
Fixxy <csszone@gmail.com>
2020-04-08 20:48:37 +0300

    style + list update

--- src/data.js
+++ src/data.js
@@ -1,10 +1,5 @@
-export let items = [
+const items_old = [
 	{
-		"img": "https://cdn1.ozone.ru/s3/multimedia-v/c1200/6006568483.jpg",
-		"name": 'Игра Клаустрафобия "Пробуждение"',
-		"price": 2000,
-		"url": "https://www.mosigra.ru/Face/Show/klaustrofobia_probuzhdenie/"
-	}, {
 		"img": "https://static-eu.insales.ru/images/products/1/1787/230344443/large_logo_new2019.jpg",
 		"name": 'Подарочный сертификат в Zero Waste Shop',
 		"price": "от 100",
@@ -15,6 +10,21 @@ export let items = [
 		"price": 4500,
 		"url": "https://hobbygames.ru/ostrov-duhov?gclid=Cj0KCQiAr8bwBRD4ARIsAHa4YyIxec6KNV3-ON3TWU7Ioxj0ZEyAlqgTn1Cxkj34tcSbPXNIjlhK-2kaAn2bEALw_wcB"
 	}, {
+		"img": "https://cdn1.ozone.ru/s3/multimedia-v/c1200/6006568483.jpg",
+		"name": 'Игра Клаустрафобия "Пробуждение"',
+		"price": 2000,
+		"url": "https://www.mosigra.ru/Face/Show/klaustrofobia_probuzhdenie/"
+	}, {
+		"img": "https://tairai.ru/upload/resize_cache/uf/d58/335_240_1/d581a12ec4173989fbeb45a8234fc0a9.jpg",
+		"name": 'Тайский массаж',
+		"price": "от 500",
+		"url": "https://tairai.ru/certificates/"
+	},
+];
+
+/* */
+export const items = [
+	 {
 		"img": "https://www.4glaza.ru/images/products/large/0/bresser-binoculars-topas-10x25-yellow-07.jpg",
 		"name": 'Бинокль для путешествий',
 		"price": 2500,
@@ -24,11 +34,6 @@ export let items = [
 		"name": 'Настольная игра Агентство Время Мадам',
 		"price": 2300,
 		"url": "https://www.crowdgames.ru/collection/time-stories-madame"
-	}, {
-		"img": "https://tairai.ru/upload/resize_cache/uf/d58/335_240_1/d581a12ec4173989fbeb45a8234fc0a9.jpg",
-		"name": 'Тайский массаж',
-		"price": "от 500",
-		"url": "https://tairai.ru/certificates/"
 	}, {
 		"img": "https://images-na.ssl-images-amazon.com/images/I/A15Yos88KUL._SX679_.jpg",
 		"name": 'Escape Room The Game',
