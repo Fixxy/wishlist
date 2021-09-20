34ffb14f3f95ca759e8d40abaa408661198757c5
Fixxy <csszone@gmail.com>
2021-09-20 12:49:31 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,12 +1,6 @@
 /* what we wanted for whatever reason */
 const itemsHidden = [
 	{
-		"img": "https://c.dns-shop.ru/thumb/st1/fit/320/250/1a0c677ecd193bf16d1ceb2cd1ea83b1/4e0d77a595e27515ec1e3d92cebb37a6a770d057f985141152087af47df0778a.jpg",
-		"name": 'Память для ноутбука',
-		"price": 5000,
-		"url": "https://www.dns-shop.ru/product/24056528a1783332/operativnaa-pamat-sodimm-amd-radeon-r7-performance-series-r7s416g2400s2s-16-gb/",
-		"person": [0]
-	}, {
 		"img": "https://hobbygames.cdnvideo.ru/image/cache/hobbygames_beta/data/HobbyWorld/Ostrov_Duhov/Photos/Ostrov_Duhov_01-1024x1024-wm.jpg",
 		"name": 'Настольная игра "Остров Духов"',
 		"price": 4500,
@@ -16,12 +10,6 @@ const itemsHidden = [
 		"name": 'Игра Клаустрафобия "Пробуждение"',
 		"price": 2000,
 		"url": "https://www.mosigra.ru/Face/Show/klaustrofobia_probuzhdenie/"
-	}, {
-		"img": "https://www.ikea.com/ru/ru/images/products/fascinera-chopping-board__0896581_PE657551_S5.JPG?f=s",
-		"name": 'Разделочная доска из Ikea',
-		"price": 800,
-		"url": "https://www.ikea.com/ru/ru/p/fasinera-razdelochnaya-doska-mangovoe-derevo-60393491/",
-		"person": [0]
 	}, {
 		"img": "https://nizagams.ru/wp-content/uploads/2019/12/%D0%90%D0%B3%D0%B5%D0%BD%D1%82%D1%81%D1%82%D0%B2%D0%BE-%D0%92%D0%A0%D0%95%D0%9C%D0%AF-%D0%9C%D0%B0%D0%B4%D0%B0%D0%BC.jpg",
 		"name": 'Настольная игра Агентство Время Мадам',
@@ -46,6 +34,12 @@ const itemsHidden = [
 		"price": 3200,
 		"url": "https://i-modelist.ru/goods/model/avto-moto/Aoshima/1045/62022.html",
 		"person": [1]
+	}, {
+		"img": "https://thumb.tildacdn.com/tild6361-6634-4361-b433-396433333164/-/resize/466x/-/format/webp/fpr-001_light_web_2_.png",
+		"name": 'Flipper Zero',
+		"price": 8000,
+		"url": "https://flipperzero.one/",
+		"person": [1]
 	}, 
 ];
 
@@ -54,12 +48,6 @@ const itemsHidden = [
 /* 0-female, 1-male */
 export const items = [
 	{
-		"img": "https://thumb.tildacdn.com/tild6361-6634-4361-b433-396433333164/-/resize/466x/-/format/webp/fpr-001_light_web_2_.png",
-		"name": 'Flipper Zero',
-		"price": 8000,
-		"url": "https://flipperzero.one/",
-		"person": [1]
-	}, {
 		"img": "https://p0.zoon.ru/preview/zZip6ZolGWaKtpTSUPAFMQ/2246x1500x75/1/0/1/original_5e5aacdf2358360eb163ad47_5eff5a11f40a2.jpg",
 		"name": 'Тайский массаж',
 		"price": "от 500",
@@ -82,7 +70,7 @@ export const items = [
 		"name": 'Крылья: Птицы Океании',
 		"price": 3000,
 		"url": "https://hobbygames.ru/krilja-ptici-okeanii",
-		"person": [0,1]
+		"person": [0]
 	},{
 		"img": "https://img-gorod.ru/28/476/2847675_detail.jpg",
 		"name": 'Книга "Solutions and Other Problems"',
